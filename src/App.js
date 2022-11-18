de20bdd76efcf41e2d3d5cd99348c81d723ef164
RutwikDhale <rutwik2514@gmail.com>
2022-11-18 23:05:20 +0530

    updated homepage

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Stock from './components/stock';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import Homepage2 from './components/homepage2';
+import Dashboard from './components/dashboard';
 import Buy from './components/buy';
 import Sell from './components/sell';
 import './App.css'
 import Login from './components/login';
 import Register from './components/register';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './Elements/Navbars/style/navbar.css'
 import './Elements/Headers/style/header.css'
-import Dashboard from './components/dashboard';
+
 function App() {
   return (
     <>
@@ -21,7 +23,7 @@ function App() {
       <Route exact path="/stock" element={<Stock/>} />
       <Route exact path="/" element={<Homepage />} />
       <Route exact path="/in" element={<Homepage2 />} />
-      <Route exact path="/user" element={<Dashboard />} />
+      <Route exact path="/dashboard" element={<Dashboard />} />
       <Route exact path="/buy" element={<Buy />} />
       <Route exact path="/sell" element={<Sell />} />
       <Route exact path="/login" element={<Login />} />
