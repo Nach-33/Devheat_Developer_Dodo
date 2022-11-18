3fce2c3d20948e5b7c945911c7839fa4c9653ae0
Nachiket <nachiketdodia33@gmail.com>
2022-11-19 00:15:55 +0530

    added database connections and schema

---
+++ App.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const userDataRoutes = require("./routes/user.data");
+const tradeRoutes = require("./routes/trade");
+const dbconnect = require("./db/connect.js");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", authRoutes);
+app.use("/api/v1", userDataRoutes);
+app.use("/api/v1", tradeRoutes);
+
+const serverStart = async () => {
+  try {
+    await dbconnect(process.env.MONGO_URI);
+    console.log("Connected to the DB");
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+serverStart();
---
+++ models/user.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    reqiured: true,
+  },
+  email: {
+    type: String,
+    reqiured: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    reqiured: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    reqiured: true,
+  },
+  portfolio: {
+    type: Array,
+    default: []
+  },
+  balance: {
+    type: Number,
+    default: 5000,
+  },
+  transactions: {
+    type: Array,
+    default: [],
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
