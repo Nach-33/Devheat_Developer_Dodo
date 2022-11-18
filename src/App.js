a67190afd474d3b141bed4afe814a49d7dce1a70
Shrey Shah <shrey.shah0610@gmail.com>
2022-11-19 00:04:35 +0530

    Home page UI Enhancements

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Homepage from './components/homepage';
-import Stock from './components/stock';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import Homepage from './components/homepage';
+import Stock from './components/stock';
 import Homepage2 from './components/homepage2';
 import Dashboard from './components/dashboard';
 import Buy from './components/buy';
 import Sell from './components/sell';
-import './App.css'
 import Login from './components/login';
 import Register from './components/register';
+import './App.css'
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './Css/header.css'
 import './Css/navbar.css'
+import './Css/feature.css'
+import './Css/features.css'
 
 
 
