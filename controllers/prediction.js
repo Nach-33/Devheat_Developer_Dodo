821be2fee511153e4adbbd9e5b6ada3abd153a77
Nachiket <nachiketdodia33@gmail.com>
2022-11-20 05:25:19 +0530

    corrected backend functionalities

--- controllers/prediction.js
+++ controllers/prediction.js
@@ -2,24 +2,30 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 const { spawn } = require("child_process");
 
-
 const predictionTrade = async (req, res) => {
   try {
     if (!req.headers.authorization.startsWith("BEARER ")) {
       res.status(404).send({ status: "not ok", msg: "invalid request" });
     }
     const token = req.headers.authorization.split(" ")[1];
-    const { price, prediction, stock } = req.body;
+    const { prediction, stock } = req.body;
     const user = jwt.verify(token, process.env.SECRET_KEY);
     const userData = await User.findOne(user);
     const date = new Date();
     if (!userData) {
       return res.status(400).send({ status: "not ok", msg: "user not found" });
     }
-    if (userData.balance < price) {
+    if (userData.balance < 50) {
       return res
         .status(400)
         .send({ status: "not ok", msg: "not enough balance" });
+    } else if (
+      userData.lastPrediction ==
+      date.getDate() + "/" + (Number(date.getMonth()) + 1)
+    ) {
+      return res
+        .status(400)
+        .send({ status: "not ok", msg: "limit exceeded" });
     } else {
       let ans = 0;
       await new Promise((resolve, reject) => {
@@ -27,6 +33,7 @@ const predictionTrade = async (req, res) => {
         childpython.stdout.on("data", (data) => {
           ans = `${data}`;
           ans = Number(ans);
+          ans = ans < 50 ? 1 : 0;
           resolve(true);
         });
         childpython.stderr.on("data", (data) => {
@@ -34,17 +41,17 @@ const predictionTrade = async (req, res) => {
           resolve(true);
         });
       }).then(() => {
-        return res.send({ status: "done", ans });
+        return res.send({ status: "done", win: ans === prediction ? 1 : 0 });
       });
-      ans=(ans<50)?1:0;
-      let reward=-50;
-      if(ans ==prediction){
-        console.log('here');
-        reward=75;
+
+      let reward = -50;
+      if (ans == prediction) {
+        console.log("here");
+        reward = 75;
       }
       const newUser = await User.findByIdAndUpdate(userData._id, {
         balance: userData.balance + reward,
-        lastPrediction: date.getDate() + "/" + (Number(date.getMonth()) + 1)
+        lastPrediction: date.getDate() + "/" + (Number(date.getMonth()) + 1),
       });
     }
   } catch (error) {
