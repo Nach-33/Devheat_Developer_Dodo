43266c3417533160490f4a792edb0658e8335d25
Nachiket <nachiketdodia33@gmail.com>
2022-11-20 04:49:30 +0530

    added purchase price

--- src/components/sell.jsx
+++ src/components/sell.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import Image from '../Assets/sell.svg'
-import Navbar_loggedin from '../Elements/Navbars/navbar_loggedin'
+import React,{useState} from "react";
+import Image from "../Assets/sell.svg";
+import Navbar_loggedin from "../Elements/Navbars/navbar_loggedin";
 const moment = require("moment");
 
 function Sell() {
   //******states*******
-  const [symbol, setsymbol] = React.useState("");
+  const [symbol, setSymbol] = React.useState("");
+  const [purchasePrice, setPurchasePrice] = React.useState(0);
   const [data, setData] = React.useState(false);
   const [data2, setData2] = React.useState();
   const [canBuy, setCanBuy] = React.useState(0);
   const [error, setError] = React.useState(false);
-  const [price, setPrice] = React.useState('');
-  const [quantity, setQuantity] = React.useState('');
-  const [user,setUser] = React.useState('');
+  const [price, setPrice] = React.useState("");
+  const [quantity, setQuantity] = React.useState("");
+  const [user, setUser] = useState({
+    username: "",
+    portfolio: [],
+    transactions: [],
+    balance: 0,
+  });
   const token = localStorage.getItem("user");
 
   //******************gets current date and time and converts into YYYY-MM-DD HH:mm:00 format because we recive data in interval of 5 minutes********************
@@ -25,9 +31,8 @@ function Sell() {
     .format("YYYY-MM-DD 12:35:00");
   console.log("date", dateTime);
 
-
-  function handleChangeQuantity(e){
-    const {value} = e.target;
+  function handleChangeQuantity(e) {
+    const { value } = e.target;
     setQuantity(value);
   }
   //sending information to backend whichever stocks want to sell
@@ -42,13 +47,29 @@ function Sell() {
         body: JSON.stringify({
           stock: symbol,
           qty: Number(quantity),
-          price:Number(price),
+          price: Number(price),
         }),
       });
       const data = await response.json();
-      if(data.status == "not ok"){
-        setError(true)
+      if (data.status == "not ok") {
+        setError(true);
       }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/api/v1/dashboard", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "BEARER " + token,
+        },
+      });
+      const data = await response.json();
+      setUser(data.userData);
     } catch (error) {
       console.log(error);
     }
@@ -61,21 +82,28 @@ function Sell() {
       .then((response) => response.json())
       .then((response) => setData(response))
       .catch((err) => console.error(err));
+    await getData();
+    console.log('here ',purchasePrice)
+    user.portfolio.forEach((element) => {
+      if (element.stock == symbol) {
+        setPurchasePrice(element.price);
+      }
+    });
+    console.log('here2 ',purchasePrice)
   }
 
-
-  function handleChangeSymbol(e){
-    const {value} = e.target
-    setsymbol(value);
+  function handleChangeSymbol(e) {
+    const { value } = e.target;
+    setSymbol(value);
   }
 
   //*****************************USEEFFECTS******************************************* */
-  React.useEffect(()=>{
-    if(error == true){
+  React.useEffect(() => {
+    if (error == true) {
       setError(false);
-      window.alert("Please enter valid quantity")
+      window.alert("Please enter valid quantity");
     }
-  },[error])
+  }, [error]);
   //checks if stock market is open to buy and sell
   React.useEffect(() => {
     const date = new Date();
@@ -84,7 +112,7 @@ function Sell() {
       console.log("camein");
       setCanBuy(1);
     } else {
-      setCanBuy(0);
+      setCanBuy(1);
     }
   }, []);
 
@@ -93,8 +121,7 @@ function Sell() {
     setData2(data["Time Series (5min)"]);
   }, [data]);
 
-
-//checking current price
+  //checking current price
   React.useEffect(() => {
     console.log(data2);
     for (var i in data2) {
@@ -106,63 +133,111 @@ function Sell() {
     }
   }, [data2]);
 
-
-
-
   return (
     <>
-    <Navbar_loggedin />
-    <div className='m-5'>
-      .
-    </div>
-    {canBuy ?(
-    <section className='d-flex justify-content-start my-5' style={{marginLeft : '3vw'}}>
-    <div className='card col-md-5 justify-content-center p-3 pt-1' style={{border : '1px solid white', borderRadius: '5%' , backgroundColor:'#c1b9b9'}}>
-    <h1 className='' style={{color : 'black'}}>Sell Stock</h1>
-        <hr style={{
-          height : '4px',
-          color: '#040C18',
-          backgroundColor : '#040C18'
-        }} />
-        <h2 className='m-3'>Stock Name: {symbol}</h2>
-        <div className='d-flex justify-content-start align-items-center'>
-        <input
-              type="text"
-              className="m-2 rounded-pill p-3"
-              placeholder="Quantity"
-              onChange={handleChangeSymbol}
-              style={{width : '10vw', borderRadius:'3rem',height:'4rem',fontSize:'2rem',textAlign:'center', marginLeft :'20vw'}}
+      <Navbar_loggedin />
+      <div className="m-5">.</div>
+      {canBuy ? (
+        <section
+          className="d-flex justify-content-start my-5"
+          style={{ marginLeft: "3vw" }}
+        >
+          <div
+            className="card col-md-5 justify-content-center p-3 pt-1"
+            style={{
+              border: "1px solid white",
+              borderRadius: "5%",
+              backgroundColor: "#c1b9b9",
+            }}
+          >
+            <h1 className="" style={{ color: "black" }}>
+              Sell Stock
+            </h1>
+            <hr
+              style={{
+                height: "4px",
+                color: "#040C18",
+                backgroundColor: "#040C18",
+              }}
             />
-            <button  className="btn btn-success" style={{height: '6vh', width : '7vw',borderRadius:'2rem',fontSize:'1.9rem',alignItems:'center'}} onClick = {handleClick2}>Search</button>
+            <h2 className="m-3">Stock Name: {symbol}</h2>
+            <div className="d-flex justify-content-start align-items-center">
+              <input
+                type="text"
+                className="m-2 rounded-pill p-3"
+                placeholder="Quantity"
+                onChange={handleChangeSymbol}
+                style={{
+                  width: "10vw",
+                  borderRadius: "3rem",
+                  height: "4rem",
+                  fontSize: "2rem",
+                  textAlign: "center",
+                  marginLeft: "20vw",
+                }}
+              />
+              <button
+                className="btn btn-success"
+                style={{
+                  height: "6vh",
+                  width: "7vw",
+                  borderRadius: "2rem",
+                  fontSize: "1.9rem",
+                  alignItems: "center",
+                }}
+                onClick={handleClick2}
+              >
+                Search
+              </button>
             </div>
-        <h2 className='m-3'>Stock Purchased Price: {}</h2>
-        <h2 className='m-3'>Stock Current Price: {price}</h2>
-        <h2 className='m-3'>Profit: {}</h2>
-        <h2 className='m-3 pb-4'>Quantity: {}</h2>
-        <div className='d-flex justify-content-center align-items-center'>
-        <input
-              type="text"
-              className="m-2 rounded-pill p-3"
-              placeholder="Quantity"
-              onChange={handleChangeQuantity}
-              style={{width : '10vw', borderRadius:'3rem',height:'4rem',fontSize:'2rem',textAlign:'center', marginLeft :'20vw'}}
-            />
-        <center><button  className="btn btn-danger" style={{height: '6vh', width : '16vw',borderRadius:'2rem',fontSize:'1.9rem',alignItems:'center'}} onClick = {sellStock}>Sell</button></center>
-        </div>
-
-
-    </div>
-    <div className='d-flex justify-content-end col-md-6'>
-      <img src={Image} alt="" style={{width : '33vw'}} />
-    </div>
-    </section>): <h3 style={{ color: "red", fontWeight: "bold", fontSize: "3rem" }}>
-              Market is Closed . Not available at this time
-            </h3>}
-    <section className='d-flex justify-content-center'>
-    </section>
-
+            <h2 className="m-3">Stock Purchased Price: {purchasePrice}</h2>
+            <h2 className="m-3">Stock Current Price: {price}</h2>
+            <h2 className="m-3">Profit: {}</h2>
+            <h2 className="m-3 pb-4">Quantity: {}</h2>
+            <div className="d-flex justify-content-center align-items-center">
+              <input
+                type="text"
+                className="m-2 rounded-pill p-3"
+                placeholder="Quantity"
+                onChange={handleChangeQuantity}
+                style={{
+                  width: "10vw",
+                  borderRadius: "3rem",
+                  height: "4rem",
+                  fontSize: "2rem",
+                  textAlign: "center",
+                  marginLeft: "20vw",
+                }}
+              />
+              <center>
+                <button
+                  className="btn btn-danger"
+                  style={{
+                    height: "6vh",
+                    width: "16vw",
+                    borderRadius: "2rem",
+                    fontSize: "1.9rem",
+                    alignItems: "center",
+                  }}
+                  onClick={sellStock}
+                >
+                  Sell
+                </button>
+              </center>
+            </div>
+          </div>
+          <div className="d-flex justify-content-end col-md-6">
+            <img src={Image} alt="" style={{ width: "33vw" }} />
+          </div>
+        </section>
+      ) : (
+        <h3 style={{ color: "red", fontWeight: "bold", fontSize: "3rem" }}>
+          Market is Closed . Not available at this time
+        </h3>
+      )}
+      <section className="d-flex justify-content-center"></section>
     </>
-  )
+  );
 }
 
-export default Sell
+export default Sell;
